e0a429f3e8a069d42e8e500451deb2db10b08a80
Rafaelbsilvaa <rafaelbezerra41@outlook.com>
2025-11-24 19:53:11 -0300

    .

--- NeoCapital/obj/Release/net8.0/NeoCapital.AssemblyInfo.cs
+++ NeoCapital/obj/Release/net8.0/NeoCapital.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("NeoCapital")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ebb9076a3fbc33cc13bdef8ec05feb03d337e89")]
 [assembly: System.Reflection.AssemblyProductAttribute("NeoCapital")]
